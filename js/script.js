af7872689eb80f3a53bef15763e6fc3d6ab7e28e
Gleb <gleblu@yandex.ru>
2020-11-23 17:14:13 +0700

    the_task_one_from_myself

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+let numberOfFilms = {};
+
+numberOfFilms = prompt("How much films you watch?", "");
+
+
+let personalMovieDB = {};
+personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+};
+
+
+let lastMovies = prompt("One of the last movies you watch?", "");
+let rateIt = prompt("How much would you rate it?", "");
+
+let lastMovies2 = prompt("One of the last movies you watch?", "");
+let rateIt2 = prompt("How much would you rate it?", "");
+
+personalMovieDB.movies[lastMovies] = rateIt;
+personalMovieDB.movies[lastMovies2] = rateIt2;
+
+console.log (personalMovieDB);
+
+
 
 
 
