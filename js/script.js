890912af0d85e24780d35828ae49e7cffaec0b14
Gleb <gleblu@yandex.ru>
2020-11-24 17:53:58 +0700

    the_task_second

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-const numberOfFilms = +prompt('How much films you watch?', '');
+const numberOfFilms = +prompt('How much films you watched?','');
 
 const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-const   a = prompt('One the last film you watched?', ''),
-        b = prompt('How much you rate it?', ''),
-        c = prompt('One the last film you watched?', ''),
-        d = prompt('How much you rate it?', '');
+for (let i = 0; i < 2; i++) {
+    const   a = prompt('One the last films you watched?',''),
+            b = prompt('How much you rate film?', '');
+    if (a !== '' && b !== '' && a !== null && b !== null && a.length < 50) {
+        console.log('done');
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log('You watched quiet a little films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You classic viewer');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You movie fan!');
+} else {
+    console.log('Error!');
+}
 
 console.log(personalMovieDB);
-
 
